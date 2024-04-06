cfff4e83b14b13c5269678b61c86dc0a9916304e
Dhananjay-3270 <shinde3270@gmail.com>
2024-04-06 21:24:32 +0530

    P1

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,27 +1,34 @@
 import { Box } from "@mui/material";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import Vector from "./assets/Vector.png";
 import Igstudio from "./assets/Igstudio.png";
-import mail from "./assets/Message 35.png"
-import styles from "./Home.module.css"
+import mail from "./assets/Message 35.png";
+import styles from "./Home.module.css";
 function Home() {
-
-
-return (
-
-
+  return (
     <>
-    <Box>
-        <Box height={990} width="100%">
-
+      <Box>
+        <Box height={890} width="100%">
           <Box display="flex" flexDirection="row" justifyContent="space-evenly">
             <Box
-              width={143} height={30.3} position="relative" alignItems="center" top={10}>
+              width={143}
+              height={30.3}
+              position="relative"
+              alignItems="center"
+              top={10}
+            >
               <img alt="logo" src={Vector} />
               <img alt="logo" src={Igstudio} />
             </Box>
 
-            <Box display="flex" flexDirection="row" justifyContent="space-evenly" width={431} height={26} color="#FFFFFF">
+            <Box
+              display="flex"
+              flexDirection="row"
+              justifyContent="space-evenly"
+              width={431}
+              height={26}
+              color="#FFFFFF"
+            >
               <h4>Home</h4>
               <h4>Attorneys</h4>
               <h4>Practice Areas</h4>
@@ -32,33 +39,45 @@ return (
               <h3>Contact Now</h3>
             </Box>
           </Box>
-          <Box  width={580} height={400}  display="flex" flexDirection="column" position="relative" top={250} left={156}>
+          <Box
+            width={580}
+            height={400}
+            display="flex"
+            flexDirection="column"
+            position="relative"
+            top={250}
+            left={156}
+          >
             <h2 className={styles.bigtext1}>
               You don’t have to Fight them Alone.
             </h2>
             <p className={styles.bigtext2}>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit blandit, curabitur sodales conubia ut inceptos faucibus himenaeos tortor eget, hac massa gravida arcu interdum proin curae.
+              Lorem ipsum dolor sit amet consectetur adipiscing elit blandit,
+              curabitur sodales conubia ut inceptos faucibus himenaeos tortor
+              eget, hac massa gravida arcu interdum proin curae.
             </p>
-<Box sx ={ {width:'438px', height :'74px' , background :'#FFFFFF', borderRadius:'43px', opacity:'10%'}}
-display='flex'
-justifyContent='center'
-alignItems='center'
->
-
-<Box  sx ={ {width:'224px', height :'26px' , background :'red', }} >
-<img alt="mail" src={mail} />
-Enter your email address 
-</Box>
-
-</Box>
+            <Box
+              sx={{
+                width: "438px",
+                height: "74px",
+                background: "#FFFFFF",
+                borderRadius: "43px",
+                opacity: "10%",
+              }}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Box sx={{ width: "224px", height: "26px", background: "red" }}>
+                <img alt="mail" src={mail} />
+                Enter your email address
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
-    
     </>
-)
-
+  );
 }
 
-
-export default Home
+export default Home;
